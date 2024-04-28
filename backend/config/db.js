6e4520c4e955ba4b4c05da94c851f34bfb3e2073
alqamah <alqama.hasnain@gmail.com>
2024-04-28 17:52:18 +0530

    added mongo-cloud config

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 export const connectDB = async () => {
   try {
     //console.log("db connecting...");
-      const res = await mongoose.connect("mongodb://127.0.0.1:27017/csvupload");
+      const res = await mongoose.connect(process.env.MONGO_URL);
     //console.log(`mongodb connected with server ${res.connection.host}`);
   } catch (error) {
     console.log("mongodb connection failed!");
